96e5fc0e5b2ea18e579cf275c0e6bb9dd078f487
Aiysha Noor <161074894+AiyshaNoor2806@users.noreply.github.com>
2024-11-01 01:52:07 +0500

    first commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,17 @@
-export default function Header() {
+import Link from "next/link";
+
+
+export default function Header(data: any) {
     return (
       <>
-      
+      <div className= {`${data.font.className}(links-parent) `}>
+        <ul className="links">
+            <li className="link"><Link href="">Works</Link></li>
+            <li className="link"><Link href="">Blog</Link></li>
+            <li className="link"><Link href="">Contact</Link></li>
+            </ul>
+        
+        </div>   
       </>
     );
   }
