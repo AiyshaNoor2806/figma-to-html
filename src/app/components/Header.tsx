8b64e55e8ba70bacd3a01cefb5e89cce68234961
Aiysha Noor <161074894+AiyshaNoor2806@users.noreply.github.com>
2024-11-01 09:43:36 +0500

    update

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
 
 
-export default function Header(data: any) {
-    return (
-      <>
-      <div className= {`${data.font.className}(links-parent) `}>
-        <ul className="links">
-            <li className="link"><Link href="">Works</Link></li>
-            <li className="link"><Link href="">Blog</Link></li>
-            <li className="link"><Link href="">Contact</Link></li>
-            </ul>
-        
-        </div>   
-      </>
-    );
-  }
-  
+interface HeaderProps {
+  font: {
+    className: string;
+  };
+}
+
+export default function Header({ font }: HeaderProps) {
+  return (
+    <div className={`${font.className} links-parent`}>
+      <ul className="links">
+        <li className="link"><Link href="/works">Works</Link></li>
+        <li className="link"><Link href="/blog">Blog</Link></li>
+        <li className="link"><Link href="/contact">Contact</Link></li>
+      </ul>
+    </div>
+  );
+}
